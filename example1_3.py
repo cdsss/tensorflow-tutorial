695d00e20c060d761cb86aaa91d1cbdbf21e6dae
ChenDS <32668922+cdsssss@users.noreply.github.com>
2018-09-11 20:19:00 +0800

    Create example1_3.py

---
+++ example1_3.py
@@ -0,0 +1,22 @@
+import tensorflow as tf
+
+# 创建一个变量，初始化为标量0
+state = tf.Variable(0, name="counter")
+
+# 创建一个op，其作用是使state增加1
+
+one = tf.constant(1)
+new_value = tf.add(state, one)
+update = tf.assign(state, new_value)
+
+# 启动图后，变量必须先经过初始化（init）op初始化
+# 首先必须增加一个初始化op到图中
+init_op = tf.global_variables_initializer()
+
+# 启动图，运行op
+with tf.Session() as sess:
+    sess.run(init_op)
+    print(sess.run(state))
+    for _ in range(3):
+        sess.run(update)
+        print(sess.run(state))
